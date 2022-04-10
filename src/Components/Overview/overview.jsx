1c4022e929095930895430d162246d8424494341
Daniel ten Damme <danieltendamme@gmail.com>
2022-04-10 16:13:23 +0200

    update resource section

--- src/Components/Overview/overview.jsx
+++ src/Components/Overview/overview.jsx
@@ -7,12 +7,19 @@ export default function Overview() {
       <div className="overview-section-s1">
         <div>
           <h1 className="h1-overview">Ethereum Resources for Developers</h1>
+          <hr className="hr" />
         </div>
-        <hr className="hr" />
+
         <p className="overview-section-p">
           BlockSource is created to help people further in their knowledge of
           Ethereum. Studying myself to become a developer in this field, I came
           across many useful resources that helped me tremendously.
+          <br />
+          <br />
+          You will find all the resources that I discovered over time. The
+          resources will be updated regularly to give developers the latest
+          available resources. As the ethereum space is constantly evolving,
+          more and more amazing tools are build.
         </p>
       </div>
     </>
--- src/Components/Resources/resources.jsx
+++ src/Components/Resources/resources.jsx
@@ -8,14 +8,77 @@ export default function Resources() {
   return (
     <>
       <section className="section1-resources">
-        <div>DOCUMENTATION</div>
-        <div>DEV TOOLS</div>
-        <div>CRYPTOGRAPHY</div>
+        <div>
+          <h1 className="section1-resources-titles">Documentation </h1>
+          <ul className="section1-resources-sources">
+            <li>
+              <a
+                href="https://ethereum.org/en/developers/docs/"
+                target="_blank"
+              >
+                Ethereum Docs
+              </a>
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="section1-resources-titles">DEV TOOLS </h1>
+          <ul className="section1-resources-sources">
+            <li>test</li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="section1-resources-titles">CRYPTOGRAPHY </h1>
+          <ul className="section1-resources-sources">
+            <li>test</li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
       </section>
       <section className="section1-resources">
-        <div>TWITTER</div>
-        <div>REDDITS</div>
-        <div>PODCASTS</div>
+        <div>
+          <h1 className="section1-resources-titles">TWITTER </h1>
+          <ul className="section1-resources-sources">
+            <li>
+              {" "}
+              <a
+                href="https://twitter.com/VitalikButerin?s=20&t=bPOdTxT3_DMCMqzxGTeLGg"
+                target="_blank"
+              >
+                Vitalik Buterin
+              </a>
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="section1-resources-titles">REDDITS </h1>
+          <ul className="section1-resources-sources">
+            <li>test</li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="section1-resources-titles">PODCASTS </h1>
+          <ul className="section1-resources-sources">
+            <li>test</li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
       </section>
       <footer className="footer-overview">test</footer>
     </>
